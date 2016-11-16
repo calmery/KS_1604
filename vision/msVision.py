5b2a21a45d1cee9c7e767072f281727ea9c6deff
Marei Kikukawa <contact@calmery.me>
2016-11-16 19:31:32 +0900

    Fix msVision.py
    Removing u' chars

--- vision/msVision.py
+++ vision/msVision.py
@@ -1,5 +1,8 @@
+# coding: utf-8
+
 import http.client
 import urllib
+import json
 
 import sys
 
@@ -9,19 +12,15 @@ headers = {
     'Content-Type': 'application/octet-stream',
     'Ocp-Apim-Subscription-Key': '264edea3f1d54a2d8d0448fbf085db85',
 }
-params = {
-    'visualFeatures': 'Description',
-} 
 
 connect = http.client.HTTPSConnection( 'api.projectoxford.ai' )
 
 img = open( file_name, 'rb' ).read()
 
 params = 'visualFeatures=Description'
-print('/vision/v1.0/analyze?%s' % params, img, headers)
 connect.request( 'POST', '/vision/v1.0/analyze?%s' % params, img, headers )
 response = connect.getresponse()
 caption_data = response.read()
 connect.close()
 
-print( caption_data )
+print(caption_data.decode())
